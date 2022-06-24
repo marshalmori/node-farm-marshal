6aac9b9a4ba777e80a7dbed861086c3c13508046
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-24 20:15:40 -0300

    Create slugs with npm slugify package

--- starter/index.js
+++ starter/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url')
 const replaceTemplate = require('./modules/replaceTemplate')
+const slugify = require('slugify')
 
 
 /////////////////////////////////////////
@@ -42,6 +43,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
     const {query, pathname } = url.parse(req.url, true);
